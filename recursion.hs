f587af76f9143e648135a6745d02f5842b21394a
Mikhail Agaltsoff <mike_a@mail.ru>
2019-06-06 19:05:37 +0700

    Recursion max

--- recursion.hs
+++ recursion.hs
@@ -1,8 +1,18 @@
 
 fibo :: Integer -> [Integer]
+
 fibo 0 = []
 fibo 1 = [0]
 fibo 2 = [0, 1]
 fibo n = reverse ((sum fibo') : reverse fibo') 
     where 
         fibo' = fibo (n - 1)
+        
+
+max' :: (Ord n) => [n] -> n
+
+max' [] = error "max' on emptyist"
+max' [x] = x
+max' (x:xs) = if x > max'' then x else max''
+    where
+        max'' = max' xs
