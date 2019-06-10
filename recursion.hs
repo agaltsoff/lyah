bec1fe4950599accd6a41a1a5eae9383f2bf23af
Mikhail Agaltsoff <mike_a@mail.ru>
2019-06-10 14:26:36 +0700

    Single fibo number function added. Fibo numbers list bug fixed.

--- recursion.hs
+++ recursion.hs
@@ -1,13 +1,17 @@
 
-fibo :: Integer -> [Integer]
+fibo :: Integer -> Integer
 
-fibo 0 = []
-fibo 1 = [0]
-fibo 2 = [0, 1]
-fibo n = reverse ((sum fibo') : reverse fibo') 
-    where 
-        fibo' = fibo (n - 1)
-        
+fibo 0 = 0
+fibo 1 = 1
+fibo n = fibo (n - 1) + fibo (n - 2)
+
+fiboo :: Integer -> [Integer]
+
+fiboo 0 = [0]
+fiboo 1 = [0, 1]
+fiboo n = reverse (( rfiboo' !! 0 + rfiboo' !! 1 ) : rfiboo') 
+    where
+        rfiboo' = reverse (fiboo (n - 1))
 
 max' :: (Ord n) => [n] -> n
 
