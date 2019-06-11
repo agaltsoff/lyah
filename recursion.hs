213bafba85f319193cdf0f8bcc76922760fc75b0
Mikhail Agaltsoff <mike_a@mail.ru>
2019-06-11 14:55:42 +0700

    Fibo list function rewritten without direct list addressing

--- recursion.hs
+++ recursion.hs
@@ -1,21 +1,17 @@
-
 fibo :: Integer -> Integer
-
 fibo 0 = 0
 fibo 1 = 1
 fibo n = fibo (n - 1) + fibo (n - 2)
 
 fiboo :: Integer -> [Integer]
-
 fiboo 0 = [0]
 fiboo 1 = [0, 1]
-fiboo n = reverse (( rfiboo' !! 0 + rfiboo' !! 1 ) : rfiboo') 
+fiboo n = reverse (sum (take 2 rfiboo') : rfiboo') 
     where
         rfiboo' = reverse (fiboo (n - 1))
 
 max' :: (Ord n) => [n] -> n
-
-max' [] = error "max' on emptyist"
+max' [] = error "max' on empty list"
 max' [x] = x
 max' (x:xs) = if x > max'' then x else max''
     where
