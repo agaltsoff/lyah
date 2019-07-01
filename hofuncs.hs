95fe4f0e8d6da1670ce7ac29328aee7ffad268c4
Mikhail Agaltsoff <mike_a@mail.ru>
2019-07-01 13:05:34 +0700

    Nuber of long collatz chains by list comprehension

--- hofuncs.hs
+++ hofuncs.hs
@@ -9,3 +9,18 @@ nply n f x = f (nply (n -1) f x)
 
 -- Test
 nply_test = nply 3 (++ "!") (nply 5 (++ "-HA") "A")
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- Collatz
+
+collatz :: (Integral a) => a -> [a]
+collatz 1 = [1]
+collatz n 
+    | even n = n : collatz (div n 2)
+    | odd n = n : collatz (n*3 + 1)
+
+numLongCollatz n =  length [head x | x <- map collatz [1..n], length x > 15] 
